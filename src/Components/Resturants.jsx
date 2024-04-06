95b57d349a827739a5756b06e0023fada1358bbd
Harsh-Srivastav123 <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-04-06 17:09:28 +0530

    Raman Code

---
+++ src/Components/Resturants.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Card from "./Card";
+import OnMind from "./OnMind";
+import ShimmerCard from "./Shimmer/ShimmerCard";
+import ShimmerOnMind from "./Shimmer/ShimmerOnMind";
+import Footer from "./Footer";
+import TopResturantChains from "./TopResturantChains";
+
+const ResturantMenu = () => {
+  const [resList, setResList] = useState([]);
+  const [apidata, setapidata] = useState(null);
+
+  const cardArray = new Array(9).fill(null);
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const fetchApi = async () => {
+    const res = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.490642&lng=80.3093933&is-seo-homepage-enabled=true"
+    );
+    const data = await res.json();
+
+    const restaurantData =
+      data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+
+    setapidata(data);
+    setResList(restaurantData);
+  };
+
+  return (
+    <>
+      <div className="mx-24 px-8">
+        {apidata !== null ? (
+          <OnMind data={apidata} />
+        ) : (
+          <div className="mx-24 px-8">
+            <ShimmerOnMind />
+          </div>
+        )}
+      </div>
+
+      <div>{apidata !== null && <TopResturantChains data={apidata} />}</div>
+
+      {resList.length === 0 && (
+        <div className="flex flex-wrap justify-center mx-8">
+          {cardArray.map((_, index) => (
+            <ShimmerCard key={index} />
+          ))}
+        </div>
+      )}
+
+      <div className="flex flex-wrap justify-center mx-8">
+        {resList.map((t) => (
+          <Link key={t.info.id} to={"/resmenu/" + t.info.id}>
+            <Card
+              image={t.info.cloudinaryImageId}
+              title={t.info.name}
+              rating={t.info.avgRating}
+              deliverytime={t.info.sla.slaString}
+              cusinis={t.info.cuisines}
+              location={t.info.areaName}
+              imageHeader={t.info.aggregatedDiscountInfoV3?.header}
+              imageSubHeader={t.info.aggregatedDiscountInfoV3?.subHeader}
+            />
+          </Link>
+        ))}
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default ResturantMenu;
+{
+  /* <div>first : What is on your mind</div>
+      <div>second: Top resturant chains</div>
+      <div>third: resturants with online food delivery.... with filters</div> */
+}
